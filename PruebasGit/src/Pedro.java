b0b5e4a92dbb307f45d685b141399d2138caf621
Federico <Federico@Federico>
2017-02-15 17:51:56 -0200

    se agrega un if

--- PruebasGit/src/Pedro.java
+++ PruebasGit/src/Pedro.java
@@ -3,6 +3,8 @@ public class Pedro {
 	public static void main (String args [])
 	{
 		System.out.println("pepe");
-		System.out.println("Diego");	
+		System.out.println("Diego");
+		if(3<4)
+			System.out.println("Diego");
 	}
 }
