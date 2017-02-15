88e5db06855b9ed344b037f040ea2590f493bcc8
Federico <Federico@Federico>
2017-02-15 17:40:30 -0200

    cambios al main de prueba

--- PruebasGit/src/Pedro.java
+++ PruebasGit/src/Pedro.java
@@ -3,12 +3,6 @@ public class Pedro {
 	public static void main (String args [])
 	{
 		System.out.println("pepe");
-		System.out.println("Diego");
-		System.out.println("pepe");
-		System.out.println("Diego");
-		System.out.println("pepe");
-		System.out.println("Diego");
-
-		
+		System.out.println("Diego");	
 	}
 }
